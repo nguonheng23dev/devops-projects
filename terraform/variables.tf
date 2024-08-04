bfdb07ca0e968f751861ce42a5b14617a106c759
Hout_Nguonheng <hout.nguonheng@cellcard.com.kh>
2024-08-04 19:06:48 +0700

    Fix deployment

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,11 +2,12 @@ variable "region" {
     default = "ap-southeast-2"
 }
 variable "public_key" {
+    default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEMpdv8XfIfVYIxDykvpd1RbPbksw4f4MKOS1AGOcbNO hnguonheng@WCISTPPHQLW795"
 
 }
 variable "private_key" {
-
+    default = "-----BEGIN OPENSSH PRIVATE KEY-----b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZWQyNTUxOQAAACBDKXb/F3yH1WCMQ8pL6XdUWz25LMOH+DCjktQBjnGzTgAAAKB2T7ogdk+6IAAAAAtzc2gtZWQyNTUxOQAAACBDKXb/F3yH1WCMQ8pL6XdUWz25LMOH+DCjktQBjnGzTgAAAEC4Fof+Hj/D7TRSwhVz5+qj5uGnSxmIqjqMvhwsiYQtcUMpdv8XfIfVYIxDykvpd1RbPbksw4f4MKOS1AGOcbNOAAAAGWhuZ3VvbmhlbmdAV0NJU1RQUEhRTFc3OTUBAgME-----END OPENSSH PRIVATE KEY-----"
 }
 variable "key_name" {
-    
+    default = ""
 }
