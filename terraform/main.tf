0bfac30af4bf3aed3dd10bee2654fd1fb031f0d0
Hout_Nguonheng <hout.nguonheng@cellcard.com.kh>
2024-08-08 13:39:17 +0700

    issues fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,43 +37,44 @@ resource "aws_iam_instance_profile" "ec2-profile" {
 }
 
 resource "aws_security_group" "maingroup" {
-    egress = [
-        {
-            cidr_blocks = ["0.0.0.0/0"]
-            description = ""
-            from_port = 53
-            ipv6_cidr_blocks = []
-            prefix_list_ids = []
-            protocol = "-1"
-            security_groups = []
-            self = false
-            to_port = 53
-        }
-    ]
-    ingress = [
-        {
-            cidr_blocks = ["0.0.0.0/0", ]
-            description = ""
-            from_port = 22
-            ipv6_cidr_blocks = []
-            prefix_list_ids = []
-            protocol = "tcp"
-            security_groups = []
-            self = false
-            to_port = 23
-        },
-        {
-            cidr_blocks = ["0.0.0.0/0", ]
-            description = ""
-            from_port = 80
-            ipv6_cidr_blocks = []
-            prefix_list_ids = []
-            protocol = "tcp"
-            security_groups = []
-            self = false
-            to_port = 80
-        }
-    ]
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0   // Update from_port to 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0   // Update to_port to 0
+    }
+  ]
+
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 23
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
+  ]
 }
 
 resource "aws_key_pair" "deployer"{
